c0c6556c979532843d276dbff65f5fef00a7c6ed
Waite <Waite.Ryan.M@dol.gov>
2017-01-13 11:40:19 -0500

    Updated 1/13/2017 v1

--- ninja.js
+++ ninja.js
@@ -1,42 +1,98 @@
-(function(win, doc){
+/*
 
-  // Error Messages
-  function error(message) {
-    this.message = message;
-    this.name = "error";
-  }
+	Ninja.JS
+	MicroFramework
+
+*/
+
+(function(window, document) {
+	'use strict';
+
+	// Error Messages
+	function error(message) {
+		this.message = message;
+		this.name = "error";
+	}
+
+	function getNJelm(ctrlName) {
+		return document.querySelector('[nj-controller="' + ctrlName + '"]');
+	}
 
 
-  // Master Ninja (Main Closure)
-  win.Ninja = function(){
-    this.ctrlObj = {};
-  }
+	// Master Ninja (Main Closure)
+	var Ninja = function() {
+		var ninja = this;
+		var body = document.querySelector('body');
+		var bodyHTML = body.innerHTML;
 
-  // Scope Object That View And Model Can Share
-  Ninja.prototype.spy = function(controlName, func) {
-    console.log(this);
-    var ctrlElm = document.querySelector('[nj-controller="' + controlName + '"]');
+		var CtrlObj = function(){
 
-    if(ctrlElm == null || ctrlElm == undefined) {
-      throw new error("No Element Found!");
-    }
+		};
 
-    this.ctrlObj = {
-      ctrlElm: ctrlElm,
-    }
+		ninja.scopes = [];
 
-    return func(this.ctrlObj);
+		// Scope Object That View And Model Can Share
+		Ninja.prototype.spy = function(ctrlName, func) {
+			// console.log(self);
 
-  }
+			for(var key in ninja.scopes) {
+				if (ninja.scopes[key].ctrlName == ctrlName) {
+					throw new error("Duplicate Controllers Are Not Allowed!");
+				}
+			}
 
-  //
-  Ninja.prototype.set = function(func) {
+			var ctrlElm = document.querySelector('[nj-controller="' + ctrlName + '"]');
 
-    var databindRegex = /\(\(([a-zA-Z0-9\s\?\&\=\#\.\_\/\-\%\\]{1,})\)\)/;
-    var body = document.querySelector('body').innerHTML;
+			if (ctrlElm == null || ctrlElm == undefined) {
+				throw new error("No DOM Element Found!");
+			}
 
-    
+			var ctrlObj = new CtrlObj();
+			ctrlObj.ctrlName = ctrlName;
+			ctrlObj.ctrlElm = ctrlElm;
+			ctrlObj.ctrlTemplate = ctrlElm.innerHTML;
 
-  }
+			ninja.scopes.push(ctrlObj);
+
+			return func(ctrlObj);
+
+		}
+
+		//
+		CtrlObj.prototype.refresh = function(func) {
+			var self = this;
+			var databindRegex = /\(\((.*?)\)\)/g;
+			var regexONE = /[a-zA-Z0-9\_\-\.]{1,}/;
+			var regexTWO = /[a-zA-Z0-9\_\-\.\(\)\{\[\}\]\'\"\s\/\\]{1,}/;
+			var dom = getNJelm(self.ctrlName);
+			var template = self.ctrlTemplate;
+
+			self.ctrlTemplate.match(databindRegex, 'h', 't').forEach(function(h, t) {
+				var val = h.replace('((', '').replace('))', '').trim().toString();
+				
+				if (self.hasOwnProperty(val)) {
+          if( typeof self[val] === 'function' ) {
+						// console.log('function');
+            template = template.replace(h , self[val]());
+          }
+          else {
+            // console.log('not function');
+            template = template.replace(h , self[val]);
+          }
+				}
+        else {
+					// console.log(false);
+				}
+			});
+
+			dom.innerHTML = template;
+
+		}
+
+
+
+	}
+
+	window.Ninja = Ninja;
 
 })(window, document);
--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 var ninja = new Ninja();
 
-ninja.spy('ninjaScript', function(scope){
+ninja.spy('ninja', function(scope){
 
   console.log(scope);
 
   scope.demo = 'yes';
 
+  scope.test = 'admit one';
+
   scope.gool = function() {
     alert('Gool');
   }
 
+  scope.func = function() {
+    return 'function';
+  }
 
+  scope.refresh();
 
-  ninja.set();
+  setTimeout(function(){
+    scope.demo = 'cool';
+    scope.refresh();
+  } , 3000)
+
+});
+
+ninja.spy('assassin', function(scope){
+
+  console.log(scope);
+
+  scope.ken = 'power';
+
+  scope.mar = 'dynamic';
+
+  scope.gool = function() {
+    alert('Gool');
+  }
+
+  scope.Genj = function() {
+    return 'Ninpo';
+  }
+
+  scope.refresh();
+
+  setTimeout(function(){
+    scope.ken = 'strike';
+    scope.refresh();
+  } , 3000)
 
 });
